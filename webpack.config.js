45922c9e549bbcf2559b048239cb5130b34e7963
geekelo <efuelight12@gmail.com>
2023-06-14 12:27:52 +0100

    Add css

--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,12 @@ const path = require('path');
      path: path.resolve(__dirname, 'dist'),
     clean: true,
    },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
  };
